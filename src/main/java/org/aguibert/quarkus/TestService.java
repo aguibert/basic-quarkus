5071a5fa17e54ea24975b2412de580e5bb6db4e3
Andrew Guibert <andy.guibert@gmail.com>
2019-10-16 13:10:23 -0500

    Add hello endpoint

---
+++ src/main/java/org/aguibert/quarkus/TestService.java
@@ -0,0 +1,17 @@
+package org.aguibert.quarkus;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class TestService {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello 2";
+    }
+
+}
